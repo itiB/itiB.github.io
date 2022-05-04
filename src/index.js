98f9d730284118a67946e328609478d1d6d8ee51
itiB <is0312vx@ed.ritsumei.ac.jp>
2022-05-04 15:23:59 +0900

    Add: top profile

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Namecard from './Namecard';
+import { Top } from './top';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Namecard />
+    <Top />
   </React.StrictMode>,
   document.getElementById('root')
 );
