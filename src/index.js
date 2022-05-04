b907861f873852dea70dbb38e3ffd9dd776e8f8a
itiB <is0312vx@ed.ritsumei.ac.jp>
2022-05-04 20:13:10 +0900

    rename files

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Nav } from './Nav';
-import { Top } from './top';
-import { Content} from './content';
+import { Top } from './Top';
+import { Content} from './Content';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
